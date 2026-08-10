8f1feea2abb86c8a6e974483506800107ce0ba06
prettyletto <danielltw2@gmail.com>
2026-08-10 14:12:14 -0300

    feat: adciona fluxo inicial de leitura do arquivo JSONL

--- cmd/batch/main.go
+++ cmd/batch/main.go
@@ -1,7 +1,62 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"io"
+	"os"
+	"text/tabwriter"
+)
+
+type Stats struct {
+	RecordsRead int64
+}
 
 func main() {
-	fmt.Println("Hello world from bigdatacorp")
+	if len(os.Args) != 2 {
+		fmt.Fprintln(os.Stderr, "usage: batch <input.jsonl>")
+		os.Exit(1)
+	}
+
+	inputPath := os.Args[1]
+
+	file, err := os.Open(inputPath)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to open input: %v\n", err)
+	}
+	defer file.Close()
+
+	reader := bufio.NewReader(file)
+
+	var stats Stats
+
+	for {
+		line, err := reader.ReadBytes('\n')
+
+		if len(line) > 0 {
+			stats.RecordsRead++
+		}
+
+		if errors.Is(err, io.EOF) {
+			break
+		}
+
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "failed to read input: %v\n", err)
+			os.Exit(1)
+		}
+	}
+
+	printStats(stats)
+}
+
+func printStats(stats Stats) {
+	w := tabwriter.NewWriter(os.Stdout, 0, 0, 2, ' ', 0)
+	defer w.Flush()
+
+	fmt.Fprintln(w, "Batch Results")
+	fmt.Fprintln(w)
+	fmt.Fprintln(w, "Count")
+	fmt.Fprintf(w, "Records Read\t%d\n", stats.RecordsRead)
 }
